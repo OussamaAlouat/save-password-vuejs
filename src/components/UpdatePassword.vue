607709f76abb2838b4b27e3e6d1cd2bb06f578a3
OussamaAlouat <al317257@uji.es>
2018-09-28 07:53:46 +0200

    Added new component to uppdate Passwords

---
+++ src/components/UpdatePassword.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-ons-page>
+        <v-ons-toolbar>
+            <div class="center">
+                Update Password
+            </div>
+            <v-ons-back-button></v-ons-back-button>
+        </v-ons-toolbar>
+        <v-ons-card>
+            <div>
+                Password
+            </div>
+            <v-ons-input type="password"
+                         v-model="password.password"></v-ons-input>
+            <div class="text">
+                Type of password
+            </div>
+            <v-ons-input placeholder="Facebook, twitter ..."
+                         type="text"
+                         v-model="password.type"></v-ons-input>
+        </v-ons-card>
+        <v-ons-button @click="updatePassword()">
+            Update
+        </v-ons-button>
+    </v-ons-page>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+export default {
+  name: 'UpdatePassword',
+  computed: {
+    ...mapGetters({
+      'password': 'getPassword'
+    })
+  },
+  methods: {
+    updatePassword () {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
