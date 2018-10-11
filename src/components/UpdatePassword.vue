e70756b1320c1d8fb254e4b89860dd1d71e91527
OussamaAlouat <al317257@uji.es>
2018-10-11 07:53:10 +0200

    Added the new password field and the new type field, and new cancel button

--- src/components/UpdatePassword.vue
+++ src/components/UpdatePassword.vue
@@ -4,14 +4,13 @@
             <div class="center">
                 Update Password
             </div>
-            <v-ons-back-button></v-ons-back-button>
         </v-ons-toolbar>
         <v-ons-card>
             <div>
                 Old password
             </div>
             <v-ons-input :type="getType()" class="textInput"
-                         v-model="password"></v-ons-input>
+                         v-model="currentPassword.password"></v-ons-input>
             <v-ons-button class="showButton" modifier="quiet"
                           @click="changeVisibilityPassword()">
                 <v-ons-icon
@@ -19,16 +18,35 @@
                         size="1.2rem, material:1rem">
                 </v-ons-icon>
             </v-ons-button>
-            <div class="text">
-                Type of password
-            </div>
 
+            <div>
+                New password
+            </div>
+            <v-ons-input :type="getType()" class="textInput"
+                         v-model="password"></v-ons-input>
+
+            <div class="text">
+                Old Type of password
+            </div>
             <v-ons-input placeholder="Facebook, twitter ..."
                          type="text"
-                         v-model="type"></v-ons-input>
+                         v-model="currentPassword.type">
+            </v-ons-input>
+
+            <div class="text">
+                New Type of password
+            </div>
+            <v-ons-input placeholder="Facebook, twitter ..."
+                         type="text"
+                         v-model="type">
+            </v-ons-input>
         </v-ons-card>
+
         <v-ons-button @click="update()">
             Update
+        </v-ons-button>
+        <v-ons-button @click="cancel()" class="cancelButton">
+            Cancel
         </v-ons-button>
         <v-ons-toast
                 :visible.sync="toastVisibility" animation="ascend">
@@ -62,17 +80,24 @@ export default {
   },
   methods: {
     ...mapActions(['updatePassword', 'goBack']),
-    start () {
-      this.password = this.currentPassword.password
-      this.type = this.currentPassword.type
-    },
     update () {
-      if (this.currentPassword.password === this.password && this.currentPassword.type === this.type) {
-        this.message = 'The old password and the new password are the same'
-        this.toastVisibility = true
-      } else {
+      if (this.checkUpdate()) {
         this.updatePassword({password: {password: this.password, type: this.type, visibility: false}})
         this.goBack()
+      }
+    },
+    checkUpdate () {
+      if (this.password === '' && this.type === '') {
+        this.message = 'The new type and the new password are empty'
+        this.toastVisibility = true
+        return false
+      } else {
+        if (this.currentPassword.password === this.password && this.currentPassword.type === this.type) {
+          this.message = 'The old password and the new password are the same'
+          this.toastVisibility = true
+          return false
+        }
+        return true
       }
     },
     getType () {
@@ -83,20 +108,24 @@ export default {
     },
     getIcon () {
       return this.isVisible ? 'fa-eye-slash' : 'fa-eye'
+    },
+    cancel () {
+      this.goBack()
     }
-  },
-  mounted () {
-    this.start()
   }
 }
 </script>
 <style>
     .textInput {
-        width: 13rem;
+        width: 13.5rem;
     }
     .showButton {
         background: white !important;
         background-color: white !important;
+    }
+    .cancelButton{
+        margin-left: 2.5rem !important;
+        background: red !important;
     }
 
 </style>
