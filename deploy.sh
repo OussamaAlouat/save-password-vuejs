e37ffabf6fdf489f98e104b4194a5d963ef47a70
OussamaAlouat <al317257@uji.es>
2019-01-05 01:10:15 +0100

    Error at url repo fixed

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:OussamaAlouat/save-password-vuejs.git master:gh-pages
+git push -f git@github.com:OussamaAlouat.github.io/save-password-vuejs.git master:gh-pages
 
 cd -
