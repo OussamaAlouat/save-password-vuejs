79a0d358ac303620856e64338461703e6361e083
OussamaAlouat <al317257@uji.es>
2021-05-14 14:18:08 +0200

    deploy

---
+++ precache-manifest.3454c389cfcf15c0be497698daddca13.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ef8ba3410dcc71578a880e7064acd7a",
+    "url": "/save-password-vuejs/fonts/fa-brands-400.2ef8ba34.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/save-password-vuejs/robots.txt"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "/save-password-vuejs/fonts/fa-solid-900.a7140145.woff"
+  },
+  {
+    "revision": "8940310ffbc1946bf5fa",
+    "url": "/save-password-vuejs/css/chunk-vendors.8f95870d.css"
+  },
+  {
+    "revision": "e6257a726a0cf6ec8c6fec22821c055f",
+    "url": "/save-password-vuejs/fonts/fa-regular-400.e6257a72.woff2"
+  },
+  {
+    "revision": "427d721b86fc9c68b2e85ad42b69238c",
+    "url": "/save-password-vuejs/fonts/fa-regular-400.427d721b.woff"
+  },
+  {
+    "revision": "dcce4b7fbd5e895561e18af4668265af",
+    "url": "/save-password-vuejs/fonts/fa-regular-400.dcce4b7f.eot"
+  },
+  {
+    "revision": "65b9977aa23185e8964b36eddbce7a20",
+    "url": "/save-password-vuejs/fonts/fa-regular-400.65b9977a.ttf"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "/save-password-vuejs/fonts/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "96f1c901c087fb64019f7665f7f8aca6",
+    "url": "/save-password-vuejs/fonts/ionicons.96f1c901.woff2"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "/save-password-vuejs/fonts/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "c37ad37a3a23417b739ac3b297416201",
+    "url": "/save-password-vuejs/fonts/ionicons.c37ad37a.woff"
+  },
+  {
+    "revision": "5e2f92123d241cabecf0b289b9b08d4a",
+    "url": "/save-password-vuejs/fonts/fa-brands-400.5e2f9212.woff2"
+  },
+  {
+    "revision": "418dad87601f9c8abd0e5798c0dc1feb",
+    "url": "/save-password-vuejs/fonts/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "8377529a07851232c9f8",
+    "url": "/save-password-vuejs/js/app.c6d95310.js"
+  },
+  {
+    "revision": "8940310ffbc1946bf5fa",
+    "url": "/save-password-vuejs/js/chunk-vendors.3876e210.js"
+  },
+  {
+    "revision": "7f9fdd5e7c0656fea97141765f3e0b50",
+    "url": "/save-password-vuejs/fonts/ionicons.7f9fdd5e.eot"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "/save-password-vuejs/fonts/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "cfdc15225683b7529d6ba1e9d8a9be59",
+    "url": "/save-password-vuejs/fonts/ionicons.cfdc1522.ttf"
+  },
+  {
+    "revision": "a7b95dbdd87e0c809570affaf366a434",
+    "url": "/save-password-vuejs/fonts/fa-brands-400.a7b95dbd.eot"
+  },
+  {
+    "revision": "98b6db59be947f563350d2284fc9ea36",
+    "url": "/save-password-vuejs/fonts/fa-brands-400.98b6db59.ttf"
+  },
+  {
+    "revision": "5eb754ab7dbd2fee562360528db4c3c0",
+    "url": "/save-password-vuejs/img/fa-regular-400.5eb754ab.svg"
+  },
+  {
+    "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
+    "url": "/save-password-vuejs/fonts/fa-solid-900.46e7cec6.eot"
+  },
+  {
+    "revision": "ff8d9f8adb0d09f11d4816a152672f53",
+    "url": "/save-password-vuejs/fonts/fa-solid-900.ff8d9f8a.ttf"
+  },
+  {
+    "revision": "665921072642ed354618b32af7425a22",
+    "url": "/save-password-vuejs/img/ionicons.66592107.svg"
+  },
+  {
+    "revision": "5bf145531213545e03ff41cd27df7d2b",
+    "url": "/save-password-vuejs/img/fa-brands-400.5bf14553.svg"
+  },
+  {
+    "revision": "49279363201ed19a840796e05a74a91b",
+    "url": "/save-password-vuejs/img/fa-solid-900.49279363.svg"
+  },
+  {
+    "revision": "747b9e9cbc60ea760c03203e57039acf",
+    "url": "/save-password-vuejs/index.html"
+  },
+  {
+    "revision": "8377529a07851232c9f8",
+    "url": "/save-password-vuejs/css/app.262c81b6.css"
+  }
+];
