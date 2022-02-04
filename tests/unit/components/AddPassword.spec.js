1ffa20e55056fa33826f23f461edfc340cdae0f7
OussamaAlouat <al317257@uji.es>
2022-02-04 13:21:19 +0100

    Test to verify addPassword function

--- tests/unit/components/AddPassword.spec.js
+++ tests/unit/components/AddPassword.spec.js
@@ -1,10 +1,54 @@
 
-import { shallowMount } from '@vue/test-utils';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
 import AddPassword from '@/components/AddPassword.vue';
+import Vuex from 'vuex';
+
+const localVue = createLocalVue()
+localVue.use(Vuex);
 
 describe('AddPassword.vue', () => {
-  it('Renders component', () => {
-    const wrapper = shallowMount(AddPassword, {})
-    expect(wrapper.isVueInstance()).toBeTruthy()
+  let store
+
+  let actions =  {
+    setPassword: jest.fn(),
+    goBack: jest.fn(),
+    save: jest.fn(),
+    setAllPasswords: jest.fn()
+  };
+
+  let getters = {
+    getTypes: () => ['Facebook', 'Gmail', 'Instagram'],
+    getPasswords: () => []
+  }
+
+  beforeEach(() => {
+    store = new Vuex.Store({
+      getters,
+      actions
+    })
   })
-})
+  it('Renders component', () => {
+    const wrapper = shallowMount(AddPassword, {
+      localVue,
+      store
+    })
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  });
+
+  it('On set a password "setPassword" will be called', () => {
+    const wrapper = shallowMount(AddPassword, {
+      localVue,
+      store
+    })
+
+    wrapper.setData({
+      password: '12222',
+      type: 'Facebook'
+    });
+
+    wrapper.vm.addPassword();
+    wrapper.vm.$nextTick();
+    expect(actions.setPassword).toHaveBeenCalled()
+  });
+
+});
