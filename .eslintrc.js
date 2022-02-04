e7b53ce911466deded38f6578b3fcd06a4f46af1
OussamaAlouat <al317257@uji.es>
2022-02-04 11:01:49 +0100

    Add config for tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
   root: true,
+
   env: {
     'node': true
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
+
   rules: {
     'vue/no-unused-vars': 'error',
     'no-unexpected-multiline': 'error',
@@ -15,7 +18,20 @@ module.exports = {
     'quotes': [2, 'single'],
     'no-var': 'error'
   },
+
   parserOptions: {
     'parser': 'babel-eslint'
-  }
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
